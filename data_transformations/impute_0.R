ae5e43a51a8bb30f82106eed12c193148e734380
Jay Swinney <jayswinney@gmail.com>
2015-07-15 23:06:33 -0500

    established dir structure

---
+++ data_transformations/impute_0.R
@@ -0,0 +1,65 @@
+# This script loads the data and cleans it
+# missing variables are replaces with
+#   - 0 if numeric, "missing" binary variable is created
+#   - "missing" level is added if factor
+#
+# script also creates training/testing split in data
+
+
+
+# read in the data to R
+# I'm using na.stings = '' to replace blanks with na
+# this also helps R read the numerical varaibles as numerical
+setwd('c:/Users/Jay/Dropbox/pred_454_team')
+df <- read.csv('data/orange_small_train.data', header = TRUE,
+               sep = '\t', na.strings = '')
+# read the target variables
+churn_ <- read.csv('data/orange_small_train_churn.labels', header = FALSE)
+appetency_ <- read.csv('data/orange_small_train_appetency.labels', header = FALSE)
+upsell_ <- read.csv('data/orange_small_train_upselling.labels', header = FALSE)
+
+churn_[churn_$V1 < 0,] <- 0
+appetency_[appetency_$V1 < 0,] <- 0
+upsell_[upsell_$V1 < 0,] <- 0
+
+# impute mising data with zeros and "missing"
+# also creates missing variable column
+for (i in names(df)){
+  vclass <- class(df[,i])
+  if(vclass == 'logical'){
+    # some of the variables are 100% missing, they are the only logical class vars
+    # so we can safely remove all logical class vars
+    df[,i] <- NULL
+  }else if(vclass %in% c('integer', 'numeric')){
+    #first check that there are missing variables
+    if(sum(is.na(df[,i])) == 0) next
+    # create a missing variable column
+    df[,paste(i,'_missing',sep='')] <- as.integer(is.na(df[,i]))
+    # fill missing variables with 0
+    df[is.na(df[,i]),i] <- 0
+  }else{
+    # gather infrequent levels into 'other'
+    levels(df[,i])[xtabs(~df[,i])/dim(df)[1] < 0.015] <- 'other'
+    # replace NA with 'missing'
+    levels(df[,i]) <- append(levels(df[,i]), 'missing')
+    df[is.na(df[,i]), i] <- 'missing'
+  }
+}
+
+# add the target variables to the data frame
+df$churn <- churn_$V1
+df$appetency <- appetency_$V1
+df$upsell <- upsell_$V1
+
+# this portion of the code should be copied exactly in every data transformation script
+# that way we will all be using the same training/testing data
+set.seed(123)
+smp_size <- floor(0.75 * nrow(df))
+train_ind <- sample(seq_len(nrow(df)), size = smp_size)
+# making a "tiny" data set so I cn quickly test r markdown and graphical paramters
+# this will be removed in the submitted version
+tiny_ind <- sample(seq_len(nrow(df)), size = floor(0.01 * nrow(df)))
+# split the data
+train <- df[train_ind, ]
+test <- df[-train_ind, ]
+tiny <- df[tiny_ind, ]
---
+++ models/churn/churn_lreg_jay.r
@@ -0,0 +1,44 @@
+# this model is a regularized logisitc regression model to predict churn
+# the regularization parameter is selected automatically using cross validation
+
+library(glmnet)
+library(ggplot2)
+
+setwd('c:/Users/Jay/Dropbox/pred_454_team')
+
+
+# choose a script to load and transform the data
+source('data_transformations/impute_0.r')
+
+# the data needs to be in matrix form, so I'm using make_mat()
+# from kdd_tools.r
+source('kdd_tools.r')
+df_mat <- make_mat(df)
+
+churn_lreg_jay <- cv.glmnet(df_mat[train_ind,],
+                           factor(train$churn), family = "binomial",
+                           nfolds = 10, type.measure = 'auc')
+
+save(churn_lreg_jay, file = 'models/churn/churn_lreg_jay.RData')
+
+# for code that would be useful for the paper highlight it like this
+# the line below lets with the dashes helps knitr find a particular chunk of
+# code to be inseterted into a r markdown document.
+# ---- churn_lreg_jay_plot ----
+# library(ggplot2)
+plot_df <- data.frame(cvm = churn_lreg_jay$cvm, cvup = churn_lreg_jay$cvup,
+                      cvlo = churn_lreg_jay$cvlo, lambda = churn_lreg_jay$lambda)
+
+ggplot(data = plot_df, aes(x = log(lambda), y = cvm)) +
+  geom_line(colour = '#d9544d', size = 1) +
+  geom_ribbon(aes(x = log(lambda), ymin = cvlo, ymax = cvup),
+              alpha=0.2, fill = '#d9544d') +
+  geom_vline(xintercept = log(churn_lreg_jay$lambda.min),
+             linetype = 3, size = 1) +
+  geom_vline(xintercept = log(churn_lreg_jay$lambda.1se),
+             linetype = 3, size = 1) +
+  ylab('AUC') + ggtitle('Cross Validation Curve Logistic Regression')
+# ----
+# line above with the dashes ends the code chunk
+
+
