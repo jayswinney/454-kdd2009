56482fa695872a8967759a4c0530f24ba34330f2
Jay Swinney <jayswinney@gmail.com>
2015-08-29 09:57:31 -0500

    upsell ensemble

--- paper/script_for_paper.r
+++ paper/script_for_paper.r
@@ -39,13 +39,9 @@ load("models/upsell/upsell_lreg_jay.RData")
 load("models/upsell/rf_jay.RData")
 load("models/upsell/upsell_nb_sandra.RData")
 load("models/upsell/upsell_rf_manjari.RData")
-load("models/upsell/upsell.knnTestPred.RData")
-load('models/upsell/upsell_rf_fullvars_fit_cp3_manjari.RData')
-load('models/upsell/upsell_rf_top25_fit_cp3_manjari.RData')
-load('models/upsell/upsell_rf_top25_oversampling_cp3_manjari.RData')
-load('models/upsell/upsell_svm_fit_cp3_manjari_prob.RData')
-load('models/upsell/upsell_svm_fit_cp3_manjari_val.RData')
-load('models/upsell/upsell_glm_fit_cp3_top25_manjari.RData')
+# load("models/upsell/upsell.knnTestPred.RData")
+load('models/upsell/upsell_rf_top25_equalsampling_manjari.RData')
+load('ensembles/upsell.RData')
 
 load("data_transformations/response.RData")
 
@@ -112,16 +108,14 @@ rm(list = c('churn_lreg_udy_predictions',
             'churn.knnTestPred'))
 
 # upsell
-upsell_df <- data.frame(upsell = test_response$upsell
-                        , logistic_regression = upsell_lreg_jay_predictions
-                        , naive_bayes = upsell_nb_sandra_predictions
-                      # , random_forest = upsell_rf_manjari_predictions
-                        , random_forest2 = upsell_rf_jay_predictions
-                        , random_forest = rf.upsell.pred.test.prob.sub)
-                      # , random_forest5 = rf.upsell.pred.test.prob.sub.over
-                      # , svm = attr(svm.upsell.pred.prob.test,
-                      #               'probabilities')[,1])
-                      # , knn = upsell.knnTestPred)
+upsell_df <- data.frame(upsell = ens_response$upsell
+                        , random_forest2 = upsell_ens_rf_jay_predictions
+                        , naive_bayes = upsell_ens_nb_sandra_predictions
+                        , random_forest = rf.upsell.pred.ensemble.equalsampling
+                        , logistic_regression = upsell_ens_lreg_jay_predictions
+                        , vote_ensemble = upsell_vote
+                        , logistic_regression_ensemble = up_logistic_ens)
+
 
 upsell_df2 <- gather(upsell_df, upsell, 'prediction')
 names(upsell_df2) <- c('true_value', 'algorithm', 'prediction')
@@ -129,12 +123,7 @@ names(upsell_df2) <- c('true_value', 'algorithm', 'prediction')
 rm(list = c('upsell_lreg_jay_predictions',
             'upsell_nb_sandra_predictions',
             'upsell_rf_manjari_predictions',
-            'upsell_rf_jay_predictions',
-            'upsell.knnTestPred',
-            'rf.upsell.pred.test.prob.prediction',
-            'rf.upsell.pred.test.prob.sub',
-            'rf.upsell.pred.test.prob.sub.over',
-            'svm.upsell.pred.prob.test'))
+            'upsell_rf_jay_predictions'))
 
 # ---- app ROC ----
 
@@ -164,7 +153,7 @@ print(xtable(make_auc(churn_df2, ens_response$churn, 0.7435)))
 
 # ---- Upsell ROC ----
 
-upsell_roc_df <- make_roc(upsell_df2, test_response$upsell)
+upsell_roc_df <- make_roc(upsell_df2, ens_response$upsell)
 # plot results
 ggplot(data = upsell_roc_df, aes(x = FPR, y = TPR, group = algorithm,
                               colour = algorithm)) +
@@ -174,4 +163,4 @@ ggplot(data = upsell_roc_df, aes(x = FPR, y = TPR, group = algorithm,
   # theme(legend.position="top")
 
 # ---- upsell AUC ----
-print(xtable(make_auc(upsell_df2, test_response$upsell, 0.8975)))
+print(xtable(make_auc(upsell_df2, ens_response$upsell, 0.8975)))
